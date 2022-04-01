cc46011f408698b42f2d2c9713b534e65c640741
정다슬 <dahseuljung@iPhonevanShiva.localdomain>
2022-04-01 21:26:32 +0200

    .

--- index_test.js
+++ index_test.js
@@ -1,8 +1,7 @@
-$(document).ready(function(){
-  $(".pins-style-door").mouseover(function(){
-    $("#rightcol ul li a").css("background-color", "black");
-  });
-  $(".pins-style-door").mouseout(function(){
-    $("#rightcol ul li a").css("background-color", "white");
+$(document).mousemove(e => {
+  $('.custom-cursor').position({
+    my: 'left center',
+    of: e,
+    collision: 'none'
   });
 });
